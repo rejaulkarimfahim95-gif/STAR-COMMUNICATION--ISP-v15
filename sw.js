b8bc224d926416c54e9ebd4f7802be6729aa5cd1
rejaulkarimfahim95-gif <rejaulkarimfahim95@gmail.com>
2026-09-02 19:27:16 +0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,8 @@
-const CACHE='star-isp-v15-1';
+const CACHE='star-isp-v16-final';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
-  const u=new URL(e.request.url);
-  if(u.origin===location.origin){
-    e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{
-      const copy=x.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return x;
-    }).catch(()=>caches.match('./index.html'))));
-  }
+ const u=new URL(e.request.url);
+ if(u.origin===location.origin)e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
 });
